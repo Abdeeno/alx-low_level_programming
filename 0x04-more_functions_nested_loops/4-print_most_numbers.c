033bf73da1d235606ab5265678ef8454034cc418
Abdeeno <92538552+Abdeeno@users.noreply.github.com>
2023-08-17 18:01:42 +0200

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,12 +1,19 @@
 #include "main.h"
-
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
+*print_most_numbers - prints numbers except 2 and 4
+*
+*Return: returns nothing
+*/
+void print_most_numbers(void)
 {
-	print_most_numbers();
-	return (0);
+int number;
+for (number = 48; number < 58; number++)
+{
+if ((number == 50) || (number == 52))
+{
+continue;
+}
+_putchar(number);
+}
+_putchar(10);
 }
